360e145895307f0b4aed95221b348ba7014d0e56
Mitchell-Borders <mitchell.borders28@gmail.com>
2022-05-29 15:57:34 -0500

    Type hints

--- game/common/items/item.py
+++ game/common/items/item.py
@@ -4,26 +4,26 @@ from game.common.game_object import GameObject
 
 class Item(GameObject):
 
-    def __init__(self, quality, value):
+    def __init__(self, quality: int, value: int):
         super().__init__()
         self.object_type = ObjectType.item
         self.quality = quality if quality >= 1 else 0
         self.value = value if value >= 1 else 0
 
     @property
-    def quality(self):
+    def quality(self) -> int:
         return self.__quality
 
     @property
-    def value(self):
+    def value(self) -> int:
         return self.__value
 
     @quality.setter
-    def quality(self, quality):
+    def quality(self, quality: int):
         self.__quality = quality if quality >= 1 else 0
 
     @value.setter
-    def value(self, value):
+    def value(self, value: int):
         self.__value = value if value >= 1 else 0
 
     def to_json(self):
@@ -32,7 +32,7 @@ class Item(GameObject):
         data['value'] = self.value
         return data
 
-    def from_json(self, data):
+    def from_json(self, data: dict) -> 'Item':
         super().from_json(data)
         self.quality = data['quality']
         self.value = data['value']
