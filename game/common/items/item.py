233d6a3a3002b5bdabf47409549272e0d7ffa4f6
Mitchell-Borders <mitchell.borders28@gmail.com>
2022-05-30 14:33:45 -0500

    Rename value to worth, quality default param

--- game/common/items/item.py
+++ game/common/items/item.py
@@ -4,37 +4,43 @@ from game.common.game_object import GameObject
 
 class Item(GameObject):
 
-    def __init__(self, quality: int, value: int):
+    def __init__(self, worth: int, quality: float = 0):
         super().__init__()
         self.object_type = ObjectType.item
-        self.quality = quality if quality >= 1 else 0
-        self.value = value if value >= 1 else 0
+        if quality > 1:
+            self.quality = 1
+        elif quality < 0:
+            self.quality = 0
+        else:
+            self.quality = quality
+        self.worth = worth if worth >= 1 else 0
 
     @property
-    def quality(self) -> int:
+    def quality(self) -> float:
         return self.__quality
 
     @property
-    def value(self) -> int:
-        return self.__value
+    def worth(self) -> int:
+        return self.__worth
 
     @quality.setter
-    def quality(self, quality: int):
-        self.__quality = quality if quality >= 1 else 0
-
-    @value.setter
-    def value(self, value: int):
-        self.__value = value if value >= 1 else 0
+    def quality(self, quality: float):
+        if quality > 1:
+            self.__quality = 1
+        elif quality < 0:
+            self.__quality = 0
+        else:
+            self.__quality = quality
 
     def to_json(self):
         data = super().to_json()
         data['quality'] = self.quality
-        data['value'] = self.value
+        data['worth'] = self.worth
         return data
 
     def from_json(self, data: dict) -> 'Item':
         super().from_json(data)
         self.quality = data['quality']
-        self.value = data['value']
+        self.worth = data['worth']
         return self
 
