b688a60f107f0d1fc08b2cf3ac34beffbca1e585
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2022-06-26 18:57:04 -0500

    Added init file (#44)
    Fixed init tests

--- game/test_suite/tests/objects/test_initialization.py
+++ game/test_suite/tests/objects/test_initialization.py
@@ -5,27 +5,19 @@ from game.common.items.item import Item
 from game.common.dispenser import Dispenser
 from game.common.map.tile import Tile
 from game.common.station import Station
+
 class TestInitialization(unittest.TestCase):
     def setUp(self):
         self.item = Item(quality=4, worth=20)
         self.station = Station(item=Item(4,20), is_infested=False)
         self.dispenser = Dispenser()
+        self.cook = Cook(action=ActionType.test, item=self.item)
+        self.tile = Tile(occupied_by= self.dispenser)
 
     def testObjectInit(self):
+        self.assertEqual(self.item.object_type, ObjectType.item)
         self.assertEqual(self.item.object_type, ObjectType.item)
         self.assertEqual(self.station.object_type, ObjectType.station)
-
-    def testDispenserInit(self):
-
-        self.item = Item(quality=4, worth=20)
-        self.cook = Cook(action=ActionType.test, item=self.item)
-        self.dispenser = Dispenser()
-        self.station = Station(item=Item(4,20), is_infested=False)
-        self.tile = Tile(occupied_by= self.dispenser)
-      
-    def testObjectInit(self):
-        self.assertEqual(self.item.object_type, ObjectType.item)
-
         self.assertEqual(self.dispenser.object_type, ObjectType.dispenser)
         self.assertEqual(self.station.object_type, ObjectType.station)
         self.assertEqual(self.tile.object_type, ObjectType.tile)
