11fda6f05bf7cad173cd3362a0aba369206f30fb
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2022-06-25 22:55:56 -0500

    Moved topping class Added topping objecttype init Fixed cook unit test

--- game/common/items/topping.py
+++ game/common/items/topping.py
@@ -4,7 +4,8 @@ from game.common.enums import *
 class Topping(Item):
 
     def __init__(self, worth: int, quality: float = 0, topping_type: int = 0, is_cut: bool=False):
-        super().__init__(self, worth, quality)
+        super().__init__(worth, quality)
+        self.object_type = ObjectType.topping
         self.topping_type = topping_type
         self.is_cut = is_cut
 
