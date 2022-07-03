6077a5f547471acfb1a99863b8f9ff480338edd1
amanda-f-ndsu <amanda.p.fetzer@ndus.edu>
2022-07-02 23:35:05 -0500

    continued logic with controller and unit tests

--- game/controllers/oven_controller.py
+++ game/controllers/oven_controller.py
@@ -1,6 +1,6 @@
 
+from game.common.stats import GameStats
 from game.controllers.controller import Controller
-from game.common.cook import Cook
 from game.common.enums import *
 from game.common.items.pizza import Pizza
 
@@ -13,16 +13,17 @@ class OvenController(Controller):
         # checks if oven is active and powered
         if oven.is_active and oven.is_powered:
             # if oven has no pizza, reset timer and set to inactive
-                if oven.held_item is None:
-                    return
+                if oven.item is None:
+                    oven.timer = GameStats.oven_timer['start']
+                    oven.is_active = False
             # if oven has pizza that is baked and has been there too long; destroy pizza
-                if oven.held_item.state == PizzaState.baked:
-                    return
+                if oven.held_item.state == PizzaState.baked and oven.timer == 0:
+                    oven.item = None
             # if oven has pizza that is finished baking, set pizza status to baked
-                if oven.timer == TimerState.finished:
-                    return
+                if oven.timer == GameStats.oven_timer['baked']:
+                    oven.item.state = PizzaState.baked
             # if oven has pizza that is still baking, subtract from timer
-                if oven.timer > TimerState.finished:
+                if oven.timer > GameStats.oven_timer['baked']:
                     return
 
    
--- game/test_suite/tests/controllers/test_oven_controller.py
+++ game/test_suite/tests/controllers/test_oven_controller.py
@@ -1,5 +1,7 @@
 import unittest
 from game.common.items.pizza import Pizza
+from game.common.items.topping import Topping
+from game.common.stats import GameStats
 from game.controllers.oven_controller import OvenController
 from game.common.enums import *
 
@@ -7,19 +9,32 @@ from game.common.enums import *
 class TestOvenController(unittest.TestCase):
     def setUp(self):
         self.ovenController = OvenController()
-        self.pizza = Pizza()
-        # self.oven = Oven()
+        self.pizza = Pizza(PizzaState.sauced)
+        self.pizza.add_topping(Topping(topping_type=ToppingType.cheese))
+        self.oven = Oven()
 
     def test_oven_burned(self):
-        pass
+        self.oven.timer = 0
+        self.pizza.state = PizzaState.baked
+        self.oven.item = self.pizza
+        self.ovenController.handle_actions(self.oven)
+        self.assertIsNone(self.oven.item)
+      
 
     def test_oven_cycle(self):
         pass
 
     def test_oven_baked(self):
-        pass
+        self.oven.timer = GameStats.oven_timer['baked']
+        self.oven.item = self.pizza
+        self.ovenController.handle_actions(self.oven)
+        self.assertEqual(self.oven.item.state, PizzaState.baked)
+        
 
     def test_oven_reset(self):
-        pass
+        self.oven.timer = GameStats.oven_timer['baked'] - 1
+        self.ovenController.handle_actions(self.oven)
+        self.assertEqual(self.oven.timer, 0)
+        
         
         
