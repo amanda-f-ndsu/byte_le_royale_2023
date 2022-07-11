5838fa98ef890adc0b6ab225ebafeec660f943bb
Carson Bring <carsonbring@gmail.com>
2022-07-11 12:10:57 -0500

    Create electrical_controller.py

---
+++ game/controllers/electrical_controller.py
@@ -0,0 +1,11 @@
+
+from game.common.stats import GameStats
+from game.controllers.controller import Controller
+from game.common.enums import *
+
+class ElectricalEvent(Controller):
+
+    def __init__(self):
+       super().__init__()
+     
+    
