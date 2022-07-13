7c1b3877982d7c97dd2e4bc65978ef70c699335c
Carson Bring <carsonbring@gmail.com>
2022-07-13 09:45:47 -0500

    initial

---
+++ game/common/stations/storage.py
@@ -0,0 +1,49 @@
+from xmlrpc.client import Boolean
+from game.common.game_object import GameObject
+from game.common.enums import ObjectType
+from game.common.items.item import Item
+
+import abc
+class Station(GameObject):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, item: Item = None, is_infested : Boolean = False):
+        super().__init__()
+        self.object_type = ObjectType.station
+        self.item: Item = item
+        self.is_infested: Boolean = is_infested
+
+    @property
+    def item(self) -> GameObject:
+        return self.__item
+
+    @property
+    def is_infested(self) -> Boolean:
+        return self.__is_infested
+    
+    @item.setter
+    def item(self, item: GameObject):
+        if(self.__item != None):
+            self.__item = item if isinstance(item, Item) else None
+
+    @is_infested.setter
+    def is_infested(self, bool: Boolean):
+        self.__is_infested = bool
+
+
+    @abc.abstractmethod
+    def take_action(self, item: Item = None):
+        return
+
+    def to_json(self) -> dict:
+        dict_data = super().to_json()
+        dict_data['item'] = self.item
+        dict_data['is_infested'] = self.is_infested
+        return dict_data
+
+    def from_json(self, data: dict) -> 'Station':
+        super().from_json(data)
+        self.item = data['item']
+        self.is_infested = data['is_infested']
+
+    
