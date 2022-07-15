d640207baec71e1749829471afdfec644a0e2094
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2022-07-15 18:25:49 -0500

    test class

--- game/common/stations/storage.py
+++ game/common/stations/storage.py
@@ -2,7 +2,15 @@ from xmlrpc.client import Boolean
 from game.common.game_object import GameObject
 from game.common.enums import ObjectType
 from game.common.items.item import Item
-import abc
+
+from game.common.enums import *
+from game.common.items.item import Item
+
+from game.common.enums import ObjectType
+from game.common.stations.station import Station
+
+
+from game.common.stations.station import Station
 class Storage(Station):
  
 
@@ -26,7 +34,9 @@ class Storage(Station):
         return item_rtn
 
     def take_action(self):
-        return self.__item
+        item_rtn = self.__item
+        self.__item == None
+        return item_rtn
 
 
     def to_json(self) -> dict:
--- game/test_suite/tests/objects/stations/test_storage.py
+++ game/test_suite/tests/objects/stations/test_storage.py
@@ -2,41 +2,29 @@ import unittest
 from game.common.enums import *
 from game.common.items.item import Item
 from game.common.items.pizza import Pizza
-from game.common.stations.oven import Oven
+from game.common.stations.storage import Storage
 
 class TestStorage(unittest.TestCase):
     def setUp(self):
         self.pizza = Pizza(state=PizzaState.sauced)
-        self.oven = Oven()
+        self.storage = Storage()
 
     def test_take_pizza(self):
         self.pizza.add_topping(ToppingType.cheese)
-        self.pizza = self.oven.take_action(self.pizza)
+        self.pizza = self.storage.take_action(self.pizza)
         self.assertIsNone(self.pizza)
-        self.assertTrue(isinstance(self.oven.item, Pizza))
+        self.assertTrue(isinstance(self.storage.item, Pizza))
 
     def test_return_pizza(self):
         item = None
         self.pizza.state = PizzaState.baked
         self.pizza.add_topping(ToppingType.cheese)
-        self.oven.item = self.pizza
-        item = self.oven.take_action(item)
-        self.assertIsNone(self.oven.item)
+        self.storage.item = self.pizza
+        item = self.storage.take_action()
+        self.assertIsNone(self.storage.item)
         self.assertTrue(isinstance(item, Pizza))
     
-    def take_pizza_false(self):
-        # pizza state is not sauced
-        self.pizza.state = PizzaState.rolled
-        self.pizza = self.oven.take_action(self.pizza)
-        self.assertIsNone(self.oven.item)
-        # pizza has no toppings
-        self.pizza.state = PizzaState.sauced
-        self.assertIsNone(self.oven.item)
-        # item is not pizza
-        item = Item(quality=4, worth=20)
-        self.pizza = self.oven.take_action(item)
-        self.assertIsNone(self.oven.item)
-
+    
 
 
 
