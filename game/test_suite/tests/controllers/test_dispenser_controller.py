64930863f04765f5d79908bc3433d099a7eca12d
amanda-f-ndsu <71937870+amanda-f-ndsu@users.noreply.github.com>
2022-09-17 20:01:01 -0500

    Dispenser Controller (#75)
    * started work on controller
    
    * moved dispenser to station folder and finished unit tests
    
    * modified controller code and added game gen to the dispenser controller unit test
    
    * code cleanup
    
    * removed outdated stats

---
+++ game/test_suite/tests/controllers/test_dispenser_controller.py
@@ -0,0 +1,24 @@
+import unittest
+from game.common.enums import *
+from game.common.map.tile import Tile
+from game.common.stations.dispenser import Dispenser
+from game.controllers.dispenser_controller import DispenserController
+from game.utils.generate_game import generate_map
+
+
+class TestDispenserController(unittest.TestCase):
+    def setUp(self):
+        self.dispenserController = DispenserController()
+        board = generate_map(5)
+        self.world = board.game_map
+    
+    def testTakeAction(self):
+        for item in self.world:
+            if(isinstance(item[6].occupied_by,Dispenser)):
+                self.assertIsNone(item[6].occupied_by.item)
+        self.dispenserController.handle_actions(self.world)
+        for item in self.world:
+            if(isinstance(item[6].occupied_by,Dispenser)):
+                self.assertIsNotNone(item[6].occupied_by.item)
+    
+   
