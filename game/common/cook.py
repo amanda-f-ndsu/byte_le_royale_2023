26ce3d7862e33249c98d35f47589adc212b60af8
amanda-f-ndsu <71937870+amanda-f-ndsu@users.noreply.github.com>
2022-09-22 17:30:56 -0500

    Movement Controller (#76)
    * added position attribute to cook, added movement enums, finished basic movement logic
    
    * finished test cases, all pass
    
    * fixed logic and unit tests; fixed ovens method
    
    * added comment

--- game/common/cook.py
+++ game/common/cook.py
@@ -7,12 +7,13 @@ from game.common.items.topping import Topping
 
 
 class Cook(GameObject):
-    def __init__(self, action: ActionType = ActionType.none, item: Item = None):
+    def __init__(self, action: ActionType = ActionType.none, item: Item = None, position = None):
         super().__init__()
         self.object_type = ObjectType.cook
         self.chosen_action = action
         self.held_item = item
         self.score = 0
+        self.position = position
 
     @property
     def chosen_action(self) -> ActionType:
@@ -26,6 +27,11 @@ class Cook(GameObject):
     def score(self) -> int:
         return self.__score
 
+    @property
+    # return format for tuple (x-position, y-position), assumes (0,0) is top left
+    def position(self) -> tuple:
+        return self.__position
+
     @chosen_action.setter
     def chosen_action(self, action: ActionType):
         self.__chosen_action = action
@@ -38,17 +44,23 @@ class Cook(GameObject):
     def score(self, score: int):
         self.__score = score
 
+    @position.setter
+    def position(self, position: tuple):
+        self.__position = position
+
     def to_json(self):
         data = super().to_json()
         data['chosen_action'] = self.chosen_action
-        data['held_item'] = self.held_item.to_json() if self.held_item is not None else None
+        data['held_item'] = self.held_item.to_json() if self.held_item else None
         data['score'] = self.score
+        data['position'] = self.position
         return data
 
     def from_json(self, data: dict) -> 'Cook':
         super().from_json(data)
         self.chosen_action = data['chosen_action']
         self.score = data['score']
+        self.position = data['position']
         temp: Item = data['held_item']
         if temp is None:
             self.held_item = None
