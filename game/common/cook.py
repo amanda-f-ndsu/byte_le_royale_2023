10492963ec2ca5fbba822812948fb571c295fc7b
Nick Hinnenkamp <93398272+NicholasHinnenkamp@users.noreply.github.com>
2022-10-06 18:15:49 -0500

    fix things

---
+++ game/common/cook.py
@@ -0,0 +1,72 @@
+from game.common.enums import ActionType, ObjectType
+from game.common.game_object import GameObject
+from game.common.items.item import Item
+from game.common.items.pizza import Pizza
+from game.common.items.topping import Topping
+
+
+
+class Cook(GameObject):
+    def __init__(self, action: ActionType = ActionType.none, item: Item = None, position = None):
+        super().__init__()
+        self.object_type = ObjectType.cook
+        self.chosen_action = action
+        self.held_item = item
+        self.score = 0
+        self.position = position
+
+
+    @property
+    def chosen_action(self) -> ActionType:
+        return self.__chosen_action
+
+    @property
+    def held_item(self) -> Item:
+        return self.__held_item
+
+    @property
+    def score(self) -> int:
+        return self.__score
+
+    @property
+    # return format for tuple (x-position, y-position), assumes (0,0) is top left
+    def position(self) -> tuple:
+        return self.__position
+
+    @chosen_action.setter
+    def chosen_action(self, action: ActionType):
+        self.__chosen_action = action
+
+    @held_item.setter
+    def held_item(self, item: Item):
+        self.__held_item = item
+
+    @score.setter
+    def score(self, score: int):
+        self.__score = score
+
+    @position.setter
+    def position(self, position: tuple):
+        self.__position = position
+
+    def to_json(self):
+        data = super().to_json()
+        data['chosen_action'] = self.chosen_action
+        data['held_item'] = self.held_item.to_json() if self.held_item else None
+        data['score'] = self.score
+        data['position'] = self.position
+        return data
+
+    def from_json(self, data: dict) -> 'Cook':
+        super().from_json(data)
+        self.chosen_action = data['chosen_action']
+        self.score = data['score']
+        self.position = data['position']
+        temp: Item = data['held_item']
+        if temp is None:
+            self.held_item = None
+        elif temp.object_type == ObjectType.pizza:
+            self.held_item = Pizza().from_json(data['held_item'])
+        else:
+            self.held_item = Topping().from_json(data['held_item'])
+        return self
---
+++ game/common/enums.py
@@ -0,0 +1,61 @@
+from enum import Enum
+
+class DebugLevel:
+    none = 0
+    client = 1
+    controller = 2
+    engine = 3
+
+class ObjectType:
+    none = 0
+    game_board = 1
+    counter = 2
+    action = 3
+    player = 4
+    item = 5
+    dispenser = 6
+    station = 7
+    cook = 8
+    topping = 9
+    tile = 10
+    pizza = 11
+    roller = 12
+    cutter = 13
+    oven = 14
+    bin = 15
+    combiner = 16
+    storage = 17
+    delivery = 18
+    sauce = 19
+
+
+
+class ActionType:
+    none = 0
+    test = 1
+
+class Move:
+    none = 0.5
+    up = 1.5
+    down = 2.5
+    left = 3.5
+    right = 4.5
+
+class PizzaState:
+    none = 0
+    rolled = 1
+    sauced = 2
+    baked = 3
+
+class ToppingType(int, Enum):
+    none = 0
+    dough = 1
+    cheese = 2
+    pepperoni = 3
+    sausage = 4
+    canadian_ham = 5
+    mushrooms = 6
+    peppers = 7
+    chicken = 8
+    olives = 9
+    anchovies = 10
