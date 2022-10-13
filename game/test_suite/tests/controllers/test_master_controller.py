2252f40cf6463cde6d4b16e7a8d0c8c02d7aa175
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2022-10-13 18:43:57 -0500

    Update test_master_controller.py

--- game/test_suite/tests/controllers/test_master_controller.py
+++ game/test_suite/tests/controllers/test_master_controller.py
@@ -1,6 +1,6 @@
 import unittest
 from game.common.enums import *
-
+from game.common.player import Player
 from game.common.stations.dispenser import Dispenser
 from game.controllers.master_controller import MasterController
 from game.utils.generate_game import generate_map
@@ -12,11 +12,13 @@ class TestDispenserController(unittest.TestCase):
         
     
     def testElectrical(self):
-        
-       self.masterController.event_active = EventType.electrical
-       self.masterController.handle_events()
-       self.masterController.current_world_data["game_map"].ovens() 
+        listofPlayers = { Player(),  Player()}
 
+        self.masterController.event_active = EventType.electrical
+        self.masterController.handle_events(listofPlayers, None)
+
+        listOfOvens = self.masterController.current_world_data["game_map"].ovens() 
+        self.assertTrue(listOfOvens[0].is_powered, False)
 
     
    
