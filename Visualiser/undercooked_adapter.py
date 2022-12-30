af2712687efe082d51b2a4c4bf56ee9ae66e7574
JohnPC <jonathan.m.rivard@ndsu.edu>
2022-12-29 21:35:27 -0600

    Added some test sprites with letters, game_map visualization for stations is working

--- Visualiser/undercooked_adapter.py
+++ Visualiser/undercooked_adapter.py
@@ -3,24 +3,45 @@ import sys
 
 class UnderCookedAdapter():
     def __init__(self, log_path):
-        log_file = open(log_path)
-        self.logs = json.loads(log_file.read())
-    
-    def print_logs(self):
-        print(self.logs)
-
-    def test_first_tick(self, output_path):
+        self.log_path = log_path
         self.output = []
-        # Scores
-        self.command(1, "add_score", [1, self.logs["1"]["clients"][0]["team_name"], 0, [400, 10]])
-        self.command(1, "add_score", [2, self.logs["1"]["clients"][1]["team_name"], 0, [400, 30]])
-        # Main floor
-        height = len(self.logs["1"]["game_map"]["game_map"])
-        width = len(self.logs["1"]["game_map"]["game_map"][0])
-        self.command(1, "add_layer", ["floor", 0, width, height])
-        self.command(1, "set_layer", ["floor", [[None, None, "floor"]]])
+
+    def test(self, output_path):
+        self.setup_scores()
+        self.setup_gamemap()
         output_file = open(output_path, 'w')
         output_file.write(json.dumps({"log": self.output}))
+
+    def setup_scores(self):
+        pass
+        #self.command(1, "add_score", [1, self.logs["1"]["clients"][0]["team_name"], 0, [400, 10]])
+        #self.command(1, "add_score", [2, self.logs["1"]["clients"][1]["team_name"], 0, [400, 30]])
+
+    def setup_gamemap(self):
+        # Load just the gamemap from game_map.json
+        game_map = open(self.log_path + "/game_map.json")
+        game_map = json.loads(game_map.read())
+        game_map = game_map["game_map"]["game_map"]
+        # Find the length and width of the gamemap
+        height = len(game_map)
+        width = len(game_map[0])
+        # Add and setup the floor layer
+        self.command(1, "add_layer", ["floor", 0, width, height])
+        self.command(1, "set_layer", ["floor", [[None, None, "floor"]]])
+        # Add and setup the station layer
+        self.command(1, "add_layer", ["stations", 1, width, height])
+        stations = []
+        # Create the list of x,y,key for the set_layer command
+        for y, row in enumerate(game_map):
+            for x, tile in enumerate(row):
+                if tile["occupied_by"] is not None:
+                    key = self.tile_key(tile["occupied_by"]["object_type"])
+                    if key != "cook":
+                        stations.append([x, y, key])
+        # Update the station layer with the list of station positions
+        self.command(1, "set_layer", ["stations", stations])
+
+        
         
     def command(self, turn, command, value):
         self.output.append(self.create_command(turn, command, value))
@@ -32,10 +53,20 @@ class UnderCookedAdapter():
             "value": value
         }
     
-    def tile_key(self, num, occupied):
+    def tile_key(self, num):
         if num == 0: return "null"
-        if num == 10 and occupied is None: return "floor"
-        if num == 10 and occupied == 2: return "counter"
+        if num == 2: return "counter"
+        if num == 6: return "dispenser"
+        if num == 8: return "cook"
+        if num == 12: return "roller"
+        if num == 13: return "cutter"
+        if num == 14: return "oven"
+        if num == 15: return "bin"
+        if num == 16: return "combiner"
+        if num == 17: return "storage"
+        if num == 18: return "delivery"
+        if num == 19: return "sauce"
+        return "NoAdapterTileKeyError"
 
 
 
@@ -48,8 +79,8 @@ def help():
 
 if(__name__ == "__main__"):
     if(len(sys.argv) == 1):
-        adapter = UnderCookedAdapter("turn_logs.json")
-        adapter.test_first_tick("graphical.json")
+        adapter = UnderCookedAdapter("./logs")
+        adapter.test("graphical.json")
     elif (len(sys.argv) == 3):
         adapter = UnderCookedAdapter(sys.argv[1])
     elif (len(sys.argv) == 2 and sys.argv[1] == "--help"):
