a1af2cfaf7a493b4bac6ca751eb9fdf7e6873be0
amanda-f-ndsu <amanda.p.fetzer@ndus.edu>
2023-01-12 19:06:03 -0600

    removed test case

--- game/test_suite/tests/controllers/test_dispenser_controller.py
+++ game/test_suite/tests/controllers/test_dispenser_controller.py
@@ -1,3 +1,4 @@
+import random
 import unittest
 
 from game.common.enums import *
@@ -22,10 +23,6 @@ class TestDispenserController(unittest.TestCase):
             if(isinstance(item[6].occupied_by,Dispenser)):
                 self.assertIsNotNone(item[6].occupied_by.item)
     
-    # def testingItems(self):
-    #     self.dispenserController.handle_actions(self.world,1)
-    #     for item in self.world.game_map:
-    #         if(isinstance(item[6].occupied_by,Dispenser)):
-    #             print(item[6].occupied_by.item.topping_type)
+    
     
    
