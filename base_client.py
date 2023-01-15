f35f11ce1a564cf0c9a7d3552743c3df89ad2f8b
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2023-01-14 21:23:53 -0600

    minor cleanup

--- base_client.py
+++ base_client.py
@@ -30,7 +30,7 @@ class Client(UserClient):
         return 'Sean\'s client'
 
     # This is where your AI will decide what to do
-    def take_turn(self, turn : int, action : Action, world : GameBoard, cook : Cook):
+    def take_turn(self, turn: int, action: Action, world: GameBoard, cook: Cook):
         """
         This is where your AI will decide what to do.
         :param turn:        The current turn of the game.
@@ -50,87 +50,98 @@ class Client(UserClient):
             case None:
                 dough_location = self.scan_board(
                     world, ObjectType.dispenser, ToppingType.dough)
-                if dough_location and self.manhattan_distance(cook.position, dough_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, dough_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                elif dough_location and self.manhattan_distance(cook.position, dough_location) == 1:
-                    action.chosen_action = ActionType.interact
-                    self.state = "Dough"
+                if dough_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, dough_location)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, dough_location)
+                    elif man_dist == 1:
+                        action.chosen_action = ActionType.interact
+                        self.state = "Dough"
             case "Dough":
-                roller_location = self.scan_board(
-                    world, ObjectType.roller)
-                if roller_location and self.manhattan_distance(cook.position, roller_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, roller_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                else:
-                    action.chosen_action = ActionType.interact
-                    self.state = "Pizza_bare"
+                roller_location = self.scan_board(world, ObjectType.roller)
+                if roller_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, roller_location)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, roller_location)
+                    else:
+                        action.chosen_action = ActionType.interact
+                        self.state = "Pizza_bare"
             case "Pizza_bare":
                 sauce_location = self.scan_board(
                     world, ObjectType.sauce)
-                if sauce_location and self.manhattan_distance(cook.position, sauce_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, sauce_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                else:
-                    action.chosen_action = ActionType.interact
-                    self.state = "Sauced"
+                if sauce_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, sauce_location)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, sauce_location)
+                    else:
+                        action.chosen_action = ActionType.interact
+                        self.state = "Sauced"
             case "Sauced":
                 combiner_location = self.scan_board(
                     world, ObjectType.combiner)
-                if combiner_location and self.manhattan_distance(cook.position, combiner_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, combiner_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                else:
-                    action.chosen_action = ActionType.interact
-                    self.state = "combiner_pizza"
-                    self.combiner_location = combiner_location
+                if combiner_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, combiner_location)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, combiner_location)
+                    else:
+                        action.chosen_action = ActionType.interact
+                        self.state = "combiner_pizza"
+                        self.combiner_location = combiner_location
             case "combiner_pizza":
                 cheese_location = self.scan_board(
                     world, ObjectType.dispenser, ToppingType.cheese)
-                if cheese_location and self.manhattan_distance(cook.position, cheese_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, cheese_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                elif cheese_location and self.manhattan_distance(cook.position, cheese_location) == 1:
-                    action.chosen_action = ActionType.interact
-                    self.state = "cut"
+                if cheese_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, cheese_location)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, cheese_location)
+                    elif man_dist == 1:
+                        action.chosen_action = ActionType.interact
+                        self.state = "cut"
             case "cut":
                 cutter = self.scan_board(
                     world, ObjectType.cutter)
-                if cutter and self.manhattan_distance(cook.position, cutter) > 1:
-                    dist_tup = self.tuple_difference(cook.position, cutter)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                elif cutter and self.manhattan_distance(cook.position, cutter) == 1:
-                    action.chosen_action = ActionType.interact
-                    self.state = "combiner_needs_cheese"
+                if cutter:
+                    man_dist = self.manhattan_distance(cook.position, cutter)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, cutter)
+                    elif man_dist == 1:
+                        action.chosen_action = ActionType.interact
+                        self.state = "combiner_needs_cheese"
             case "combiner_needs_cheese":
-                if  self.combiner_location and self.manhattan_distance(cook.position, self.combiner_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position,  self.combiner_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
+                man_dist = self.manhattan_distance(
+                    cook.position, self.combiner_location)
+                if man_dist > 1:
+                    action.chosen_action = self.move_action(
+                        cook.position, self.combiner_location)
                 else:
                     action.chosen_action = ActionType.interact
                     self.state = "combiner_has_cheese"
             case "combiner_has_cheese":
                 action.chosen_action = ActionType.interact
                 self.state = "uncooked"
-                self.combiner_location = None
             case "uncooked":
                 oven = self.scan_board(
                     world, ObjectType.oven)
-                if oven and self.manhattan_distance(cook.position, oven) > 1:
-                    dist_tup = self.tuple_difference(cook.position, oven)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                else:
-                    action.chosen_action = ActionType.interact
-                    self.oven = oven
-                    self.state = "wait"
+                if oven:
+                    man_dist = self.manhattan_distance(cook.position, oven)
+                    if man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, oven)
+                    else:
+                        action.chosen_action = ActionType.interact
+                        self.oven = oven
+                        self.state = "wait"
             case "wait":
                 if world.game_map[self.oven[0]][self.oven[1]].occupied_by.item.state == PizzaState.baked:
                     action.chosen_action = ActionType.interact
@@ -138,14 +149,21 @@ class Client(UserClient):
             case "finished_pizza":
                 delivery_location = self.scan_board(
                     world, ObjectType.delivery)
-                if delivery_location and self.manhattan_distance(cook.position, delivery_location) > 1:
-                    dist_tup = self.tuple_difference(cook.position, delivery_location)
-                    direction_to_move = self.decide_move(dist_tup)
-                    action.chosen_action = direction_to_move
-                else:
-                    action.chosen_action = ActionType.interact
-                    self.state = None
+                if delivery_location:
+                    man_dist = self.manhattan_distance(
+                        cook.position, delivery_location)
+                    if delivery_location and man_dist > 1:
+                        action.chosen_action = self.move_action(
+                            cook.position, delivery_location)
+                    else:
+                        action.chosen_action = ActionType.interact
+                        self.state = None
         pass
+
+    def move_action(self, cook_position, station_location) -> ActionType.Move:
+        dist_tup = self.tuple_difference(cook_position, station_location)
+        direction_to_move = self.decide_move(dist_tup)
+        return direction_to_move
 
     def scan_board(self, world: GameBoard, object_type: ObjectType, topping_type: ToppingType = None) -> Tuple[int, int]:
         for y in range(0, len(world.game_map)):
@@ -155,20 +173,22 @@ class Client(UserClient):
                         return (y, x)
         return None
 
-    def manhattan_distance(self, int_tuple_one: Tuple[int, int], int_tuple_two : Tuple[int, int]) -> int:
+    def manhattan_distance(self, int_tuple_one: Tuple[int, int], int_tuple_two: Tuple[int, int]) -> int:
         return abs(int_tuple_one[0] - int_tuple_two[0]) + abs(int_tuple_one[1] - int_tuple_two[1])
 
-    def tuple_difference(self, int_tuple_one: Tuple[int, int], int_tuple_two : Tuple[int, int]) -> int:
-        y_diff = (int_tuple_one[0] - max(min(int_tuple_two[0], self.y_max), self.y_min))
-        x_diff = (int_tuple_one[1] - max(min(int_tuple_two[1], self.x_max), self.x_min))
+    def tuple_difference(self, int_tuple_one: Tuple[int, int], int_tuple_two: Tuple[int, int]) -> int:
+        y_diff = (
+            int_tuple_one[0] - max(min(int_tuple_two[0], self.y_max), self.y_min))
+        x_diff = (
+            int_tuple_one[1] - max(min(int_tuple_two[1], self.x_max), self.x_min))
         return (y_diff, x_diff)
 
-    def decide_move(self, int_tuple : Tuple[int, int]) -> ActionType.Move:
+    def decide_move(self, int_tuple: Tuple[int, int]) -> ActionType.Move:
         if int_tuple[1] > 0:
             return ActionType.Move.left
         elif int_tuple[1] < 0:
             return ActionType.Move.right
         elif int_tuple[0] > 0:
             return ActionType.Move.up
-        elif int_tuple[0] < 0: 
+        elif int_tuple[0] < 0:
             return ActionType.Move.down
