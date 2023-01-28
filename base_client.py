27b254db8914061a878e47713c950323814f1d6a
JohnPC <jonathan.m.rivard@ndsu.edu>
2023-01-27 23:24:12 -0600

    Simple base clients with pydoc support

--- base_client.py
+++ base_client.py
@@ -15,8 +15,10 @@ class State:
 
 # Main client class
 class Client(UserClient):
-    # Variables and info you want to save between turns go here
     def __init__(self):
+        """
+        Variables and info you want to save between turns go here
+        """
         super().__init__()
         self.state = State.WAITING_FOR_DOUGH
         # What side and x and y bounds are we using (set in start)
@@ -32,9 +34,8 @@ class Client(UserClient):
 
         :returns:       Your team name
         """
-        return 'Base Client 1'
+        return 'Base client 1'
 
-    # Start function
     def start(self, action: Action, world: GameBoard, cook: Cook):
         """
         Run on the first turn by the take_turn() function
@@ -44,7 +45,6 @@ class Client(UserClient):
         """
         self.check_side(cook)
 
-    # This is where your AI will decide what to do
     def take_turn(self, turn: int, action: Action, world: GameBoard, cook: Cook):
         """
         This is where your bot will decide what to do.
@@ -92,10 +92,14 @@ class Client(UserClient):
                     self.state = State.WAITING_FOR_DOUGH
 
     def check_side(self, cook):
+        """
+        Check which side the cook is on and set bounds and is_left_side
+
+        :param cook:        Pass your cook for its position
+        """
         if self.is_left_side != None:
             return
         if cook.position[1] < 6:
-            print("Left")
             self.is_left_side = True
             self.x_min = 1
             self.x_max = 5
@@ -103,22 +107,27 @@ class Client(UserClient):
             self.is_left_side = False
             self.x_min = 7
             self.x_max = 11
-            print("Right")
 
     def move_action(self, cook_position: Tuple[int, int], target_location: Tuple[int, int]) -> ActionType.Move:
         """
         Determines which direction to move, and returns that ActionType
+
+        :param cook_position:       The cook position in [y, x]
+        :param target_location:     The target position in [y, x]
+        :returns ActionType.Move:   Will return the correct action type to move towards the bounded target position
         """
         dist_tup = self.move_difference(cook_position, target_location)
         direction_to_move = self.decide_move(dist_tup)
         return direction_to_move
 
-    def scan_board(self, world: GameBoard, station_type: ObjectType, item_type_eval_func=None) -> Tuple[int, int]:
+    def scan_board(self, world: GameBoard, station_type: ObjectType, eval_func=None) -> Tuple[int, int]:
         """
-        Scans every tile on your clients half of the gameboard for a certain ObjectType enum
-        topping_type_eval_func is a function that takes an item and returns True if you want that item
-        If topping_type_eval_func isn't provided, it will just return the first Object found 
-        Returns the position of what you were searching for
+        Scans every tile on your clients half of the gameboard for a station that matches station_type and the eval_func
+
+        :param world:               GameBoard to search
+        :param station_type:        ObjectType to filter stations by
+        :param eval_func:           Lambda function to filter by, will be ignored if None
+        :returns Tuple[int, int]:   Will return the y,x position of what you were searching for
         """
         # For each tile on your side
         for y in range(self.y_min-1, self.y_max+2):
@@ -128,10 +137,10 @@ class Client(UserClient):
                 if station != None and station.object_type == station_type:
                     item = station.item
                     # If not eval function, return the station
-                    if item_type_eval_func == None:
+                    if eval_func == None:
                         return (y, x)
                     # Check eval function on item
-                    elif item_type_eval_func(station):
+                    elif eval_func(station):
                         return (y, x)
         # Didn't find anything that matched our criteria
         return None
@@ -139,38 +148,46 @@ class Client(UserClient):
     def manhattan_distance(self, int_tuple_one: Tuple[int, int], int_tuple_two: Tuple[int, int]) -> int:
         """
         See https://en.wikipedia.org/wiki/Taxicab_geometry
+
+        :param int_tuple_one:   First y,x position
+        :param int_tuple_two:   Second y,x position
+        :returns int:           Returns Manhatten Distance of two y,x positions
         """
         return abs(int_tuple_one[0] - int_tuple_two[0]) + abs(int_tuple_one[1] - int_tuple_two[1])
 
-    def move_difference(self, int_tuple_one: Tuple[int, int], int_tuple_two: Tuple[int, int]) -> int:
+    def move_difference(self, int_tuple_one: Tuple[int, int], int_tuple_two: Tuple[int, int]) -> Tuple[int, int]:
         """
-        Returns the difference between two tuples
-        Pinned to your side of the gameboard
+        Returns the difference between a cook position and a bounded position
+        The second position is bounded by your side of the game board
+
+        :returns Tuple[int, int]:   Will return the y, x difference of two positions
         """
-        print(str(int_tuple_one) + " : " + str(int_tuple_two), end = "\t\t")
 
-        y = max(min(int_tuple_two[1], self.y_max), self.y_min)
-        x = max(min(int_tuple_two[0], self.x_max), self.x_min)
+        # Check for left side bounds
+        if self.is_left_side:
+            x = max(min(int_tuple_two[1], self.x_max), self.x_min)
+        # Check for right side bounds
+        else:
+            x = min(max(int_tuple_two[1], self.x_min), self.x_max)
+        # Y bounds are the same for left and right side
+        y = max(min(int_tuple_two[0], self.y_max), self.y_min)
 
-        print(str(int_tuple_one) + " : " + str((y, x)), end="\t\t")
-
-        y_diff = int_tuple_one[1] - y
-        x_diff = int_tuple_one[0] - x
+        # Find the difference between them and return it
+        y_diff = int_tuple_one[0] - y
+        x_diff = int_tuple_one[1] - x
         return (y_diff, x_diff)
 
     def decide_move(self, tuple_diff: Tuple[int, int]) -> ActionType.Move:
         """
         Decides which direction to move in, based on a tuple difference
+
+        :returns ActionType.Move:   Will return the correct action type to move towards the tuple difference
         """
         if tuple_diff[1] > 0:
-            print("L")
             return ActionType.Move.left
         elif tuple_diff[1] < 0:
-            print("R")
             return ActionType.Move.right
         elif tuple_diff[0] > 0:
-            print("U")
             return ActionType.Move.up
         elif tuple_diff[0] < 0:
-            print("D")
             return ActionType.Move.down
